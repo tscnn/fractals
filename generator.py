9fc3eca762af4ee8ef7f3e7bf7289f880a2b489c
tscnn <tobias.schomann@gmail.com>
2015-08-24 18:50:30 +0200

    new feature: parameter 'rotate' added, it rotates the image by a user-given angle

--- generator.py
+++ generator.py
@@ -20,13 +20,14 @@ iterations = int(arguments['iterations'].value)
 constants = set(list(str(arguments['constants'].value).decode("hex")))
 axiom = str(arguments['axiom'].value).decode("hex")
 rules = str(arguments['rules'].value).decode("hex").split("\n")
+rotate = float(arguments['rotate'].value)
 border = 100
 width = 820
 bgcolor = "#404040"
 
 subject = lsys.subject(axiom)
 result = subject.replace([lsys.rule(rule) for rule in rules], iterations)
-points, kinds = result.track(angle, constants)
+points, kinds = result.track(angle, constants, start_rotation=rotate)
 points = points / points[:,0].max() * (width - border) + border/2
 height = points[:,1].max() + border/2
 
